d097ac6a89dee3b644f98da3f21048ec4ae27383
himanshu-antal <himanshuantal26@gmail.com>
2026-02-23 02:11:59 +0530

    src service updae

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,9 +1,30 @@
-import React from "react"
+
 import axios from "axios"
 
 const api = axios.create({
+   // baseURL:"http://localhost/5000/api"
   baseURL: "https://vehicle-tracker-lila.onrender.com/api",
-  withCredentials: true,
 })
+//handle 400
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token")
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
+//handle 401
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token")
+      window.location.href = "/login"
+    }
+    return Promise.reject(error)
+  }
+)
 
 export default api
